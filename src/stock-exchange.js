1443239ffc67c664756a836928b1c855a163c357
Igor Minar <igor@angular.io>
2017-10-24 21:52:35 +0200

    Step 1: replace the kitchen sink import with naked imports
    Improves size, but the imports still monkey-patch prototype, which results
    in problems with code-splitting and nightmare of maintaining unused imports.
    
        main.fcf1531a1a4a3d85a65c.js   149 kB       0  [emitted]  main

--- src/stock-exchange.js
+++ src/stock-exchange.js
@@ -1,4 +1,8 @@
-import {Observable} from 'rxjs';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+
 
 export const stockExchange = {
   getFeed(tickers) {
